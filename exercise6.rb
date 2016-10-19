f32f32cac6807683e8a8a35619616b38441477d6
David Wood <davidnmwood@gmail.com>
2016-10-19 14:37:10 -0500

    added rice

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,8 @@
-ary = ['coffee', 'bread', 'salad mix', 'honey', 'donuts']
+ary = ['* coffee', '* bread', '* salad mix', '* honey', '* donuts']
 grocery_list = ary
+grocery_list << "* rice"
+grocery_list.each do |list|
+  puts list
+end
+
+#
