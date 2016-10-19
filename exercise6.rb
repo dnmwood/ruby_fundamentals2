66a62b45e37679d69d8becb39765af65ce879b35
David Wood <davidnmwood@gmail.com>
2016-10-19 15:06:06 -0500

    added astericks

--- exercise6.rb
+++ exercise6.rb
@@ -1,8 +1,16 @@
-ary = ['* coffee', '* bread', '* salad mix', '* honey', '* donuts']
-grocery_list = ary
-grocery_list << "* rice"
-grocery_list.each do |list|
-  puts list
+grocery_list = ['coffee', 'bread', 'salad mix', 'honey', 'donuts']
+
+def print_list(list) #Jacob helped me with this part after my first commit.
+  list.each do |num|
+    puts "* #{num}"
+  end
 end
 
-#
+grocery_list << "rice"
+grocery_list.each do |list|
+  puts list
+  sleep(0.1)
+
+end
+
+puts "Total number of items: #{grocery_list.count}"
