5147859db00fa4d363083183475f5e163cb95b12
David Wood <davidnmwood@gmail.com>
2016-10-19 16:06:46 -0500

    second item on list

--- exercise6.rb
+++ exercise6.rb
@@ -5,18 +5,21 @@ def print_list(list) #Jacob helped me with this part after my first commit.
     puts "* #{num}"
   end
 end
+#
+# grocery_list << "rice"
+# grocery_list.each do |list|
+#   puts list
+#   sleep(0.1)
+# end
 
-grocery_list << "rice"
-grocery_list.each do |list|
-  puts list
-  sleep(0.1)
-end
+puts grocery_list[1]
 
-if grocery_list.include?("Bananas")
-  puts "You don't need to pick up bananas today."
-else
-  puts "You need to pick up bananas today."
-end
-
-sleep(1)
-puts "Total number of items: #{grocery_list.count}"
+#
+# if grocery_list.include?("Bananas")
+#   puts "You don't need to pick up bananas today."
+# else
+#   puts "You need to pick up bananas today."
+# end
+#
+# sleep(0.7)
+# puts "Total number of items: #{grocery_list.count}"
