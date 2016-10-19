e0b5167a9ca0e0ec909ef00697e0661714db2a9b
David Wood <davidnmwood@gmail.com>
2016-10-19 16:30:24 -0500

    delete my donuts

--- exercise6.rb
+++ exercise6.rb
@@ -9,17 +9,20 @@ end
 grocery_list << "rice"
 
 grocery_list.sort!
-print_list(grocery_list)
+# print_list(grocery_list)
 # grocery_list.sort.each do |list|
 #   puts list
 #   sleep(0.1)
 # end
+#
+grocery_list.delete("donuts")
+print_list(grocery_list)
 
-if grocery_list.include?("Bananas")
-  puts "You don't need to pick up bananas today."
-else
-  puts "You need to pick up bananas today."
-end
-
-sleep(0.7)
-puts "Total number of items: #{grocery_list.count}"
+# if grocery_list.include?("Bananas")
+#   puts "You don't need to pick up bananas today."
+# else
+#   puts "You need to pick up bananas today."
+# end
+#
+# sleep(0.7)
+# puts "Total number of items: #{grocery_list.count}"
